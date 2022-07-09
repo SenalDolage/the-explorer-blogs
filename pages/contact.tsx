c37d7a11ca10c8f08f08fdc30c07129a1cf00352
Senal <senal.dolage@gmail.com>
2022-07-10 02:00:05 +0530

    sample about, contact pages

---
+++ pages/contact.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import DefaultLayout from "../layouts/default-layout";
+
+const Contact: NextPage = () => {
+    return (
+        <div className="home">
+            <DefaultLayout pageTitle="About">
+                <div className="bg-yellow py-20 lg:py-6 min-h-[250px] flex items-center w-full">
+                    <div className="container">
+                        <div className="mb-6 text-4xl leading-10 font-semibold text-center">
+                            Contact
+                        </div>
+                    </div>
+                </div>
+            </DefaultLayout>
+        </div>
+    )
+}
+
+export default Contact
