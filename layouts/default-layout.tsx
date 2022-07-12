43eb992e2ac5fcb3e4a4589d78cc68385b8a7df9
Senal <senal.dolage@gmail.com>
2022-07-12 20:32:58 +0530

    main added to dafault layout

--- layouts/default-layout.tsx
+++ layouts/default-layout.tsx
@@ -10,7 +10,9 @@ export default function DefaultLayout({ children, pageTitle }: Props) {
     return (
         <div className="default-layout">
             <Header title={pageTitle} />
-            {children}
+            <main>
+                {children}
+            </main>
             <Footer />
         </div>
     )
